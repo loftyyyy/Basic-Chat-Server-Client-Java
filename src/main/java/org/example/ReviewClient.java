e9e515fa5463cc2562a3adb2409beb8ac12f4cba
loftyyyy <lofty08.business@gmail.com>
2024-06-15 22:14:02 +0800

    hello

--- src/main/java/org/example/ReviewClient.java
+++ src/main/java/org/example/ReviewClient.java
@@ -1,12 +1,31 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.net.InetSocketAddress;
+import java.nio.channels.SocketChannel;
+
 public class ReviewClient {
+
+    private SocketChannel socketChannel;
+    private Reader reader;
+    private BufferedReader bufferedReader;
 
     public static void main(String[] args){
         new ReviewClient().start();
     }
 
     public void start(){
+
+        try {
+            socketChannel = SocketChannel.open();
+            socketChannel.bind(new InetSocketAddress(5000));
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
