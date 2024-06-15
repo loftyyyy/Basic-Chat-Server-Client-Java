71b08fda38d233c6761856a75b64204bfa78eca4
loftyyyy <lofty08.business@gmail.com>
2024-06-15 22:11:33 +0800

    hi

--- src/main/java/org/example/ReviewClient.java
+++ src/main/java/org/example/ReviewClient.java
@@ -1,4 +1,12 @@
 package org.example;
 
 public class ReviewClient {
+
+    public static void main(String[] args){
+        new ReviewClient().start();
+    }
+
+    public void start(){
+
+    }
 }
